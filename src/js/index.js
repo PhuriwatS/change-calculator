d1137e0a4c038be2e881c353430bf86638729729
Pond <pond.sunday@hotmail.com>
2019-01-16 21:21:32 +0700

    Install node-sass and change folder structure

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './index.scss'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="home-container">
+        <div className="home-header">
+          <p>Change calculator</p>
+        </div>
+        <div className="home-content">
+          <p>Change calculator</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
